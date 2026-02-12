67a40562798297dfee329ddc3272765982013de9
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-12 01:22:23 +0100

    Fix bug to 768px

--- src/assets/components/currentWeather/CurrentWeather.jsx
+++ src/assets/components/currentWeather/CurrentWeather.jsx
@@ -2,84 +2,88 @@ import React, { useState, useEffect } from "react";
 import Compass from "../../resources/icons/Compass.jsx";
 import GetWeatherIcon from "../iconMap/IconMap.jsx";
 import useWeatherService from "../../services/WeatherService.jsx";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { translateCondition } from "../translate/TranslateCondition.jsx";
-import './currentWeather.scss';
+import "./currentWeather.scss";
 
 const CurrentWeather = () => {
-    const { t, i18n } = useTranslation();
-    const [weather, setWeather] = useState(null);
-    const { process, getPoznanWeather, setProcess } = useWeatherService();
+  const { t, i18n } = useTranslation();
+  const [weather, setWeather] = useState(null);
+  const { process, getPoznanWeather, setProcess } = useWeatherService();
 
-    useEffect(() => {
-        onRequest();
-    }, []);
+  useEffect(() => {
+    onRequest();
+  }, []);
 
-    const onRequest = () => {
-        getPoznanWeather()
-            .then(data => {
-                setWeather(data);
-                setProcess('confirmed');
-            })
-            .catch(err => console.error("Error loading weather:", err));
-    };
+  const onRequest = () => {
+    getPoznanWeather()
+      .then((data) => {
+        setWeather(data);
+        setProcess("confirmed");
+      })
+      .catch((err) => console.error("Error loading weather:", err));
+  };
 
-    if (!weather) return <span className="current-weather__subtitle">{t('loading')}</span>;
+  if (!weather)
+    return <span className="current-weather__subtitle">{t("loading")}</span>;
 
-    return (
-        <div className="current-weather">
-            <h2 className="current-weather__title">{t('current')}</h2>
+  return (
+    <div className="current-weather">
+      <h2 className="current-weather__title">{t("current")}</h2>
 
-            <div className="current-weather__wrapper">
-
-                <div className="current-weather__secondary current-weather__secondary--stats">
-                    <h3 className="current-weather__subtitle">{t('about')}</h3>
-                    <ul className="current-weather__stats">
-                        <li className="current-weather__stat">
-                            <span>{t('feel')}:</span>
-                            <span>{weather.feelslike} °C</span>
-                        </li>
-                        <li className="current-weather__stat">
-                            <span>{t('rain')}:</span>
-                            <span>{weather.rain}%</span>
-                        </li>
-                        <li className="current-weather__stat">
-                            <span>{t('windspeed')}:</span>
-                            <span>{weather.windspeed} {t('kmh')}</span>
-                        </li>
-                        <li className="current-weather__stat">
-                            <span>{t('humidity')}:</span>
-                            <span>{weather.humidity}%</span>
-                        </li>
-                        <li className="current-weather__stat">
-                            <span>{t('visibility')}:</span>
-                            <span>{weather.visibility} {t('km')}</span>
-                        </li>
-                        <li className="current-weather__stat">
-                            <span>{t('index')}:</span>
-                            <span>{weather.uvindex}</span>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="current-weather__primary">
-                    <GetWeatherIcon />
-                    <p className="current-weather__temperature">
-                        {t('temperature')}: {weather.temperature} °C
-                    </p>
-                    <p className="current-weather__condition">
-                        {t('condition')}: {translateCondition(weather.condition, i18n.language)}
-                    </p>
-                </div>
-
-                <div className="current-weather__secondary current-weather__secondary--wind">
-                    <h3 className="current-weather__subtitle">{t('wind')}</h3>
-                    <Compass />
-                </div>
-
-            </div>
+      <div className="current-weather__wrapper">
+        <div className="current-weather__secondary current-weather__secondary--stats">
+          <h3 className="current-weather__subtitle">{t("about")}</h3>
+          <ul className="current-weather__stats">
+            <li className="current-weather__stat">
+              <span>{t("feel")}:</span>
+              <span>{weather.feelslike} °C</span>
+            </li>
+            <li className="current-weather__stat">
+              <span>{t("rain")}:</span>
+              <span>{weather.rain}%</span>
+            </li>
+            <li className="current-weather__stat">
+              <span>{t("windspeed")}:</span>
+              <span>
+                {weather.windspeed} {t("kmh")}
+              </span>
+            </li>
+            <li className="current-weather__stat">
+              <span>{t("humidity")}:</span>
+              <span>{weather.humidity}%</span>
+            </li>
+            <li className="current-weather__stat">
+              <span>{t("visibility")}:</span>
+              <span>
+                {weather.visibility} {t("km")}
+              </span>
+            </li>
+            <li className="current-weather__stat">
+              <span>{t("index")}:</span>
+              <span>{weather.uvindex}</span>
+            </li>
+          </ul>
         </div>
-    );
-}
+
+        <div className="current-weather__primary">
+          <GetWeatherIcon />
+          <p className="current-weather__temperature">
+            {t("temperature")}: {weather.temperature} °C
+          </p>
+          <p className="current-weather__condition">
+            {t("condition")}:{" "}
+            {translateCondition(weather.condition, i18n.language)}
+          </p>
+        </div>
+
+        <div className="current-weather__secondary current-weather__secondary--wind">
+          <h3 className="current-weather__subtitle">{t("wind")}</h3>
+          <Compass />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CurrentWeather;
--- src/assets/components/forecast/Forecast.jsx
+++ src/assets/components/forecast/Forecast.jsx
@@ -2,52 +2,53 @@ import React, { useState, useEffect } from "react";
 
 import GetWeatherIcon from "../iconMap/IconMap.jsx";
 import useWeatherService from "../../services/WeatherService.jsx";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { translateDay } from "../translate/TranslateDay.jsx";
 import { translateCondition } from "../translate/TranslateCondition.jsx";
 
-import './forecast.scss';
+import "./forecast.scss";
 
 const Forecast = () => {
-    const { t, i18n } = useTranslation();
-    const [days, setDays] = useState([]);
-    const { getForecastWeather, setProcess } = useWeatherService();
+  const { t, i18n } = useTranslation();
+  const [days, setDays] = useState([]);
+  const { getForecastWeather, setProcess } = useWeatherService();
 
-    useEffect(() => {
-        getForecastWeather()
-            .then(data => {
-                setDays(data);
-                setProcess("confirmed");
-            })
-            .catch(err => console.error("Error loading weather:", err));
-    }, []);
+  useEffect(() => {
+    getForecastWeather()
+      .then((data) => {
+        setDays(data);
+        setProcess("confirmed");
+      })
+      .catch((err) => console.error("Error loading weather:", err));
+  }, []);
 
-    if (days.length === 0)
-        return <span className="current-weather__subtitle">{t('loading')}</span>;
+  if (days.length === 0)
+    return <span className="current-weather__subtitle">{t("loading")}</span>;
 
-    return (
-        <div className="forecast">
-            <h2 className="forecast__title">{t('forecast')}</h2>
-            <div className="forecast__wrapper">
-
-                {days.map((day, index) => (
-                    <div className="forecast__details" key={index}>
-                        <div className="forecast__border">
-                            <p className="forecast__day">{translateDay(day.day, i18n.language)}</p>
-                            <GetWeatherIcon />
-                            <p className="forecast__temp">
-                                {t('temperature')}: {day.temperature}°C
-                            </p>
-                            <p className="forecast__condition">
-                                {t('condition')}: {translateCondition(day.condition, i18n.language)}
-                            </p>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="forecast">
+      <h2 className="forecast__title">{t("forecast")}</h2>
+      <div className="forecast__wrapper">
+        {days.map((day, index) => (
+          <div className="forecast__details" key={index}>
+            <div className="forecast__border">
+              <p className="forecast__day">
+                {translateDay(day.day, i18n.language)}
+              </p>
+              <GetWeatherIcon />
+              <p className="forecast__temp">
+                <span>{t("temperature")}:</span> {day.temperature}°C
+              </p>
+              <p className="forecast__condition">
+                <span>{t("condition")}: </span>
+                {translateCondition(day.condition, i18n.language)}
+              </p>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Forecast;
-
